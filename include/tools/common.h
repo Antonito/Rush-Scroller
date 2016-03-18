cf891351ba2c4ad460d4c6583d7fb6a9b735d18d
Antoine Baché <antoine.bache@epitech.eu>
2016-03-19 00:49:29 +0100

    Added RADIAN and DEGREE macro

--- include/tools/common.h
+++ include/tools/common.h
@@ -5,21 +5,23 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Fri Mar 18 22:40:48 2016 Antoine Baché
-** Last update Fri Mar 18 23:45:56 2016 Antoine Baché
+** Last update Sat Mar 19 00:49:00 2016 Antoine Baché
 */
 
 #ifndef	COMMON_H_
 # define COMMON_H_
 # define MALLOC(X)	my_malloc(X)
 
+# include <stdlib.h>
+# include <lapin.h>
+
 /*
 ** Maths macros
 */
+# define RADIAN(X)	(((X) * M_PI) / 180)
+# define DEGREE(X)	(((X) * 180) / M_PI)
 # define POW2(X)	((X) * (X))
 # define POW3(X)	(POW2(X) * (X))
-
-# include <stdlib.h>
-# include <lapin.h>
 
 # ifndef DEBUG
 void			*my_malloc(size_t);
