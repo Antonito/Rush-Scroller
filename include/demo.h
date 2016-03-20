db6c165e8746a26d020a157bc6c82c6ac27a17ab
Antoine Baché <antoine.bache@epitech.eu>
2016-03-20 14:29:09 +0100

    Fixed segfault
    Former-commit-id: 4adc303b053a485e95a55dd68127e77feb0ab0eb

--- include/demo.h
+++ include/demo.h
@@ -5,7 +5,7 @@
 ** Login   <bache_a@epitech.net>
 **
 ** Started on  Fri Mar 18 21:36:02 2016 Antoine Baché
-** Last update Sun Mar 20 12:24:47 2016 Ludovic Petrenko
+** Last update Sun Mar 20 14:24:02 2016 Antoine Baché
 */
 
 #ifndef	DEMO_H_
@@ -14,7 +14,7 @@
 # define WIN_X		1920
 # define WIN_Y		1080
 # define WIN_NAME	"Rush Scroller"
-# define NB_CONTEXT	11
+# define NB_CONTEXT	12
 
 # include <lapin.h>
 
--- include/tunnel.h
+++ include/tunnel.h
@@ -5,11 +5,12 @@
 ** Login   <petren_l@epitech.net>
 **
 ** Started on  Sun Mar 20 00:41:06 2016 Ludovic Petrenko
-** Last update Sun Mar 20 03:30:00 2016 Ludovic Petrenko
+** Last update Sun Mar 20 14:15:31 2016 Antoine Baché
 */
 
 #ifndef TUNNEL_H_
 # define TUNNEL_H_
+# define TUNNEL_MUSIC	"assets/music/trainer.ogg"
 
 # include <lapin.h>
 # include "tools/transform.h"
@@ -20,6 +21,12 @@ typedef struct	s_circle
   t_ivec2	pos;
   int		color;
 }		t_circle;
+
+typedef struct	s_tunnel
+{
+  t_circle	*c;
+  t_bunny_music	*music;
+}		t_tunnel;
 
 t_bunny_response	tunnelKey(t_bunny_event_state,
 				  t_bunny_keysym,
